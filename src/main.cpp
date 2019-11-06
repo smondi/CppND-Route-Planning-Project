d0b42a5232c854102803332c9825e161e832274b
Sassan Mondi <sassan.mondi@gmail.com>
2019-11-06 00:14:32 -0500

    Reading start and end point coordinators.

--- src/main.cpp
+++ src/main.cpp
@@ -55,12 +55,25 @@ int main(int argc, const char **argv)
     // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
+    float start_x = 0.0f;
+    float start_y = 0.0f;
+    float end_x = 0.0f;
+    float end_y = 0.0f;
+
+    std::cout << "Enter Start Point X Coordinate: ";
+    std::cin >> start_x;
+    std::cout << "Enter Start Point Y Coordinate: ";
+    std::cin >> start_y;
+    std::cout << "Enter End Point X Coordinate: ";
+    std::cin >> end_x;
+    std::cout << "Enter End Point Y Coordinate: ";
+    std::cin >> end_y;
 
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
