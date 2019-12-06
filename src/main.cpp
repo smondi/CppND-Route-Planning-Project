3a458bdae8ec5006993c333d4262bcede16206e1
Sassan Mondi <sassan.mondi@gmail.com>
2019-12-06 00:15:51 -0500

    Removing debugging statements from main

--- src/main.cpp
+++ src/main.cpp
@@ -88,7 +88,6 @@ int main(int argc, const char **argv)
 
     // Create RoutePlanner object and perform A* search.
     RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
-    // RoutePlanner route_planner{model, 10.0, 10.0, 90.0, 90.0};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
